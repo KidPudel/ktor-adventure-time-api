ba19b48402a693d379c5b11f28035a3e05251a21
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-12-28 16:21:24 +0300

    added more images + added Jake

--- src/main/kotlin/com/example/routes/CharactersRoute.kt
+++ src/main/kotlin/com/example/routes/CharactersRoute.kt
@@ -15,7 +15,19 @@ private val characters = listOf(
         17,
         "Male",
         listOf("Mathematical!", "Whoa! Algebraic!"),
-        "$BASE_URL/characters_images/finn_pixel.png"
+        "$BASE_URL/characters_images/finn.png"
+    ),
+    Character(
+        "Jake the Dog",
+        "Jake",
+        "Dog/Shape-shifter hybrid",
+        16,
+        "Male",
+        listOf(
+            "Girl, you smell good. Did you take a bath in rainbows and cupcakes?",
+            "Being crazy is hard. You’re getting all hung up, all hung up on imaginary problems. You gotta focus on what’s real, man."
+        ),
+        "$BASE_URL/characters_images/finn.png"
     ),
     Character(
         "Simon Petrikov",
@@ -24,7 +36,7 @@ private val characters = listOf(
         1046,
         "Male",
         listOf("Ladies! I brought you a baby, and a puppy!", "I love you too . . . Gunter."),
-        "$BASE_URL/characters_images/finn_pixel.png"
+        "$BASE_URL/characters_images/finn.png"
     )
 )
 
@@ -34,6 +46,6 @@ fun Route.charactersRoute() {
         call.respond(
             status = HttpStatusCode.OK,
             characters
-            )
+        )
     }
 }
