e9bb8381834858af03179f082ad086eee7f842cf
Iggy Kuperson <I.Kupchinenko@gmail.com>
2023-01-09 18:35:35 +0300

    added more characters

--- src/main/kotlin/com/example/common/Constants.kt
+++ src/main/kotlin/com/example/common/Constants.kt
@@ -1,6 +1,77 @@
 package com.example.common
 
+import com.example.data.models.Character
+
 object Constants {
     // you can find it in terminal with ipconfig for windows and ifconfig for linux and unix and port in application.conf
-    const val BASE_URL = "http://192.168.1.8:8080"
+    const val BASE_URL = "http://192.168.0.112:8080"
+
+    val characters: List<Character> = listOf(
+        Character(
+            "Finn Mertens",
+            "Finn",
+            "Human",
+            17,
+            "Male",
+            listOf("Mathematical!", "Whoa! Algebraic!"),
+            "$BASE_URL/characters_images/finn.png"
+        ),
+        Character(
+            "Jake the Dog",
+            "Jake",
+            "Dog/Shape-shifter hybrid",
+            16,
+            "Male",
+            listOf(
+                "Girl, you smell good. Did you take a bath in rainbows and cupcakes?",
+                "Being crazy is hard. You’re getting all hung up, all hung up on imaginary problems. You gotta focus on what’s real, man."
+            ),
+            "$BASE_URL/characters_images/jake.png"
+        ),
+        Character(
+            "Simon Petrikov",
+            "Ice King",
+            "Human/Ice Wizard",
+            1046,
+            "Male",
+            listOf("Ladies! I brought you a baby, and a puppy!", "I love you too . . . Gunter."),
+            "$BASE_URL/characters_images/ice_king.png"
+        ),
+        Character(
+            "Lumpy Space Princess",
+            "LSP",
+            "Lumps Anti-Elemental",
+            19,
+            "Female",
+            listOf(
+                "WHATEVERS 2009!",
+                "First you should check out my house. It's, like, kinda lame but way less lame than, like, your house."
+            ),
+            "$BASE_URL/characters_images/lsp.png"
+        ),
+        Character(
+            "Be MOre",
+            "BMO",
+            "MO",
+            1006,
+            "Male",
+            listOf(
+                "Check, please.",
+                "Finn, you're dumb- you're dumb- you're dumb- you're da man! This is gonna be a snap!"
+            ),
+            "$BASE_URL/characters_images/ice_king.png"
+        ),
+        Character(
+            "Bonnibel \"Bonnie\" Bubblegum",
+            "PB",
+            "Candy Elemental",
+            830,
+            "Female",
+            listOf(
+                "Check, please.",
+                "Finn, you're dumb- you're dumb- you're dumb- you're da man! This is gonna be a snap!"
+            ),
+            "$BASE_URL/characters_images/pb.png"
+        )
+    )
 }
--- src/main/kotlin/com/example/routes/CharactersRoute.kt
+++ src/main/kotlin/com/example/routes/CharactersRoute.kt
@@ -1,5 +1,6 @@
 package com.example.routes
 
+import com.example.common.Constants
 import com.example.common.Constants.BASE_URL
 import com.example.data.models.Character
 import io.ktor.http.*
@@ -7,45 +8,13 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-private val characters = listOf(
-    Character(
-        "Finn Mertens",
-        "Finn",
-        "Human",
-        17,
-        "Male",
-        listOf("Mathematical!", "Whoa! Algebraic!"),
-        "$BASE_URL/characters_images/finn.png"
-    ),
-    Character(
-        "Jake the Dog",
-        "Jake",
-        "Dog/Shape-shifter hybrid",
-        16,
-        "Male",
-        listOf(
-            "Girl, you smell good. Did you take a bath in rainbows and cupcakes?",
-            "Being crazy is hard. You’re getting all hung up, all hung up on imaginary problems. You gotta focus on what’s real, man."
-        ),
-        "$BASE_URL/characters_images/finn.png"
-    ),
-    Character(
-        "Simon Petrikov",
-        "Ice King",
-        "Human/Ice Wizard",
-        1046,
-        "Male",
-        listOf("Ladies! I brought you a baby, and a puppy!", "I love you too . . . Gunter."),
-        "$BASE_URL/characters_images/finn.png"
-    )
-)
 
 fun Route.charactersRoute() {
     // add a http method to GET characters
     get("/characters") {
         call.respond(
             status = HttpStatusCode.OK,
-            characters
+            message = Constants.characters
         )
     }
 }
